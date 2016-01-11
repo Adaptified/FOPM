ad89ce8904bf36bd449b847efd5e7de6e5f784fc
Chaosiity (Git) <hentscheljr1@aol.com>
2016-01-10 19:33:06 -0500

    Initial commit

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_links.java
@@ -0,0 +1,20 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH, blockHostConsole = true)
+@CommandParameters(description = "All server links", usage = "/<command>")
+public class Command_links extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+       sender.sendMessage(ChatColor.GREEN + "Website: http://altimaxfreedom.com");
+       sender.sendMessage(ChatColor.GREEN + "Github: N/A");
+       sender.sendMessage(ChatColor.GREEN + "Forums: http://altimaxfreedom.com/forums");
+        return true;
+    }
+}
